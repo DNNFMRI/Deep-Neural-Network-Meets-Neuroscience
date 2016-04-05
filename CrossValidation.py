393af1b2efcc1514cbe98cf2988180bae2f85c4b
freddiezhang <fredzhang0509@gmail.com>
2016-04-04 21:00:02 -0400

    more cross validation

--- CrossValidation.py
+++ CrossValidation.py
@@ -20,7 +20,7 @@ def CV_onsub():
 
     batch_size = 128
     nb_classes = 12
-    nb_epoch = 12
+    nb_epoch = 32
 
     # input image dimensions
     dimx = 51
@@ -32,39 +32,6 @@ def CV_onsub():
     nb_pool = 2
     # convolution kernel size
     nb_conv = 3
-
-
-
-    model = Sequential()
-
-
-    # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv,
-    #                         border_mode='valid',
-    #                         input_shape=(1, dimx, dimy, dimz)))
-
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                            border_mode='valid',
-                            input_shape=(dimx, dimy, dimz), dim_ordering='tf'))
-
-    model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
-
-    # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv))
-
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-    # model.add(MaxPooling3D(pool_size=(nb_pool, nb_pool, nb_pool)))
-
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
 
     testscore = 0.0
     testaccuracy = 0.0
@@ -86,8 +53,38 @@ def CV_onsub():
         Y_test = np_utils.to_categorical(Y_test, nb_classes)
 
 
+        model = Sequential()
+
+        # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv,
+        #                         border_mode='valid',
+        #                         input_shape=(1, dimx, dimy, dimz)))
+
+        model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                                border_mode='valid',
+                                input_shape=(dimx, dimy, dimz), dim_ordering='tf'))
+
+        model.add(Activation('relu'))
+        model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+
+        # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv))
+
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+        # model.add(MaxPooling3D(pool_size=(nb_pool, nb_pool, nb_pool)))
+
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(nb_classes))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+
         model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-                  show_accuracy=True, verbose=1)
+                  show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
         score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
         print('Test score in CV ' + str(i) + ':', score[0])
         print('Test accuracy in CV ' + str(i)  + ':', score[1])
@@ -98,3 +95,95 @@ def CV_onsub():
     print('Average Test score:', testscore/9)
     print('Average Test accuracy:', testaccuracy/9)
 
+
+def CV_onsample(path="data-P2.mat", test_split=0.1, nb_test = 12):
+    print('CV on samples...')
+    batch_size = 128
+    nb_classes = 12
+    nb_epoch = 32
+
+    # input image dimensions
+    dimx = 51
+    dimy = 61
+    dimz = 23
+    # number of convolutional filters to use
+    nb_filters = 65
+    # size of pooling area for max pooling
+    nb_pool = 2
+    # convolution kernel size
+    nb_conv = 3
+
+    X, Y = getData.load_dataAll(path)
+
+    nb_data = X.shape[0]
+
+    nb_CV = nb_data / nb_test
+
+    X = X.astype('float32')
+    Y = np_utils.to_categorical(Y, nb_classes)
+
+    testscore = 0.0
+    testaccuracy = 0.0
+
+
+    for i in range(0, nb_CV):
+        model = Sequential()
+
+        # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv,
+        #                         border_mode='valid',
+        #                         input_shape=(1, dimx, dimy, dimz)))
+
+        model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                                border_mode='valid',
+                                input_shape=(dimx, dimy, dimz), dim_ordering='tf'))
+
+        model.add(Activation('relu'))
+        model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+
+        # model.add(Convolution3D(nb_filters, nb_conv, nb_conv, nb_conv))
+
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+        # model.add(MaxPooling3D(pool_size=(nb_pool, nb_pool, nb_pool)))
+
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(nb_classes))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+
+        if i == 0:
+            # continue
+            X_test = X[: nb_test]
+            Y_test = Y[: nb_test]
+            X_train = X[nb_test: ]
+            Y_train = Y[nb_test: ]
+        elif i == nb_CV - 1:
+            X_test = X[i*nb_test: ]
+            Y_test = Y[i*nb_test: ]
+            X_train = X[: i*nb_test]
+            Y_train = Y[: i*nb_test]
+        else:
+            X_test = X[i*nb_test: (i+1)*nb_test]
+            Y_test = Y[i*nb_test: (i+1)*nb_test]
+            X_train = np.concatenate((X[: i*nb_test], X[(i+1)*nb_test: ]))
+            Y_train = np.concatenate((Y[: i*nb_test], Y[(i+1)*nb_test: ]))
+        model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+                  show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
+        score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+        print('Test score in CV round ' + str(i) + ':', score[0])
+        print('Test accuracy in CV round ' + str(i)  + ':', score[1])
+        testscore = testscore + score[0]
+        testaccuracy = testaccuracy + score[1]
+
+    print('Average Test score:', testscore/nb_CV)
+    print('Average Test accuracy:', testaccuracy/nb_CV)
+
+
+CV_onsample("data-P3.mat")
+# CV_onsub()
